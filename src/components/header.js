0cdef968bc2b659c550f42292f3803f089963976
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-09-23 12:28:53 +0430

    Book store initial

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => (
+  <nav className="header">
+    <div className="header-1">
+      <h1 className="logo">Bookstore CMS</h1>
+    </div>
+    <div className="menu">
+      <Link className="menu-link" to="/">
+        Books
+      </Link>
+      <Link className="menu-link" to="/categories">
+        Categories
+      </Link>
+    </div>
+    <div>
+      <img src="../images/profile.jpg" alt="Profile Icon" />
+    </div>
+  </nav>
+);
+
+export default Header;
