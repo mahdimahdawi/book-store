c866bf0df4c2e3abaab78745b1297ddf077dd56d
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-09-28 10:03:38 +0430

    fix Linter

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,7 +1,7 @@
-import { checkStatus } from '../redux/categories/categories';
 import React from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
+
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
   const dispatch = useDispatch();
