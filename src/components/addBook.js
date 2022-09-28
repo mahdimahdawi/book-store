a43081eec43de9a973d3625eb5af9682e524127e
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-09-28 22:23:00 +0430

    Book Store version 3

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,25 +1,61 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <div className="add-book">
-    <form>
-      <input
-        type="text"
-        className="input"
-        name="title"
-        placeholder="Enter Book Title..."
-      />
-      <input
-        type="text"
-        className="input"
-        name="author"
-        placeholder="Enter Book Author..."
-      />
-      <button type="submit" className="submit-btn">
-        Add Book
-      </button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [book, setBook] = useState({
+    id: '',
+    title: '',
+    author: '',
+    action: '',
+    completed: 0,
+    currentChp: '',
+  });
+  const handleEvent = (event) => {
+    setBook({
+      ...book,
+      id: uuid().split('-')[4],
+      [event.target.name]: [event.target.value],
+    });
+  };
+  const handleSave = (event) => {
+    if (book.title !== '' && book.author !== '') {
+      dispatch(addBook(book));
+      navigate('/');
+    }
+  };
+  return (
+    <div className='add-book'>
+      <form>
+        <input
+          type='text'
+          className='input'
+          name='title'
+          placeholder='Enter Book Title...'
+          onChange={(event) => {
+            handleEvent(event);
+          }}
+        />
+        <input
+          type='text'
+          className='input'
+          name='author'
+          placeholder='Enter Book Author...'
+          onChange={(event) => {
+            handleEvent(event);
+          }}
+        />
+        <button type='button' className='submit-btn' onClick={handleSave}>
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
